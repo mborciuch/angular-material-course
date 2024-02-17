b5d1f56d72596c52e501f99d080a84a938e9b3c5
mboriuch <michal123borciuch@gmail.com>
2024-02-17 16:51:51 +0100

    Create Course Form

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
 
 
 @Component({
@@ -22,10 +23,21 @@ export class CreateCourseStep1Component {
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
 
+  protected dateClass: MatCalendarCellClassFunction<Date> = (cell, view) => {
+    // Only highligh dates inside the month view.
+    if (view === 'month') {
+      const date = cell.getDate();
+
+      // Highlight the 1st and 20th day of each month.
+      return date === 1 ? 'highlight-date' : '';
+    }
+
+    return '';
+  }
+
   constructor(private fb: UntypedFormBuilder) {
 
   }
-
   get courseTitle() {
     return this.form.controls['title'];
   }
