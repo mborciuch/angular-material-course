985c7e6d41972c51e186f76b80ecec5c301f158d
mboriuch <michal123borciuch@gmail.com>
2024-02-26 08:37:50 +0100

    Course component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,118 +1,101 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { MatTableDataSource } from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, Sort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
+import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
+import {merge, fromEvent, throwError} from "rxjs";
+import {Lesson} from "../model/lesson";
+import {SelectionModel} from "@angular/cdk/collections";
 
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.scss']
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-    course:Course;
+  @ViewChild(MatPaginator)
+  matPaginator: MatPaginator;
 
-    lessons = [
-       {
-        id: 120,
-        'description': 'Introduction to Angular Material',
-        'duration': '4:17',
-        'seqNo': 1,
-        courseId: 11
-      },
-      {
-        id: 121,
-        'description': 'Navigation and Containers',
-        'duration': '6:37',
-        'seqNo': 2,
-        courseId: 11
-      },
-      {
-        id: 122,
-        'description': 'Data Tables',
-        'duration': '8:03',
-        'seqNo': 3,
-        courseId: 11
-      },
-      {
-        id: 123,
-        'description': 'Dialogs and Overlays',
-        'duration': '11:46',
-        'seqNo': 4,
-        courseId: 11
-      },
-      {
-        id: 124,
-        'description': 'Commonly used Form Controls',
-        'duration': '7:17',
-        'seqNo': 5,
-        courseId: 11
-      },
-      {
-        id: 125,
-        'description': 'Drag and Drop',
-        'duration': '8:16',
-        'seqNo': 6,
-        courseId: 11
-      },
-      {
-        id: 126,
-        'description': 'Responsive Design',
-        'duration': '7:28',
-        'seqNo': 7,
-        courseId: 11
-      },
-      {
-        id: 127,
-        'description': 'Tree Component',
-        'duration': '11:09',
-        'seqNo': 8,
-        courseId: 11
-      },
-      {
-        id: 128,
-        'description': 'Virtual Scrolling',
-        'duration': '3:44',
-        'seqNo': 9,
-        courseId: 11
-      },
-      {
-        id: 129,
-        'description': 'Custom Themes',
-        'duration': '8:55',
-        'seqNo': 10,
-        courseId: 11
-      },
-      {
-        id: 130,
-        'description': 'Changing Theme at Runtime',
-        'duration': '12:37',
-        'seqNo': 11,
-        courseId: 11
-      }
-    ];
+  @ViewChild(MatSort)
+  matSort: MatSort
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
+  course: Course;
+  displayedColumns: string[] = ['select', 'seqNo', 'description', 'duration']
 
+  lessons: Lesson[];
+
+  isLoading: boolean = false;
+  expandedLesson: Lesson = null;
+
+  selection: SelectionModel<Lesson> = new SelectionModel<Lesson>(true, []);
+
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService) {
+
+  }
+
+  ngOnInit() {
+    this.course = this.route.snapshot.data["course"];
+    this.loadDataFromServer();
+
+  }
+
+  ngAfterViewInit() {
+    this.matSort.sortChange.subscribe((value: Sort) => this.matPaginator.pageIndex = 0)
+
+    merge(this.matSort.sortChange, this.matPaginator.page)
+    .pipe().subscribe(result => {
+      this.loadDataFromServer();
+    });
+
+  }
+
+  toggleLesson(lesson: Lesson){
+    if(lesson === this.expandedLesson){
+      this.expandedLesson = null;
+    } else {
+      this.expandedLesson = lesson;
     }
+  }
 
-    ngOnInit() {
+  toggleSelection(lesson: Lesson){
+    this.selection.toggle(lesson)
+  }
 
-        this.course = this.route.snapshot.data["course"];
+  private loadDataFromServer() {
+    this.isLoading = true;
+    this.coursesService.findLessons(this.course.id,
+      this.matSort?.direction ?? 'asc',
+      this.matPaginator?.pageIndex ?? 0,
+      this.matPaginator?.pageSize ?? 3,
+      this.matSort?.active ?? 'seqNo'
+      )
+      .pipe(
+        tap(value => this.lessons = value),
+        catchError((err: unknown) => {
+          alert(err);
+          return throwError(err);
+        }),
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe();
+  }
 
-
+  isAllSelected() {
+   return this.selection.selected?.length === this.lessons.length;
+  }
+  selectAll(){
+    if(this.isAllSelected()){
+      this.selection.clear();
+    } else {
+      this.selection.select(...this.lessons)
     }
-
-    ngAfterViewInit() {
-
-
-    }
-
+  }
 }
